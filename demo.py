184d554bbe0f3f48bb8fd594c3194511dd80598c
wzp <zhipangwang@gmail.com>
2023-04-18 11:44:15 +0800

    feat(.): update demo
    complete demo

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from msedge.selenium_tools import EdgeOptions, Edge
 
 
+# set up the driver
 edge_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 edgedriver_location = r"D:\msedgedriver.exe"
 
@@ -11,16 +12,16 @@ options.binary_location = edge_location
 driver = Edge(options=options, executable_path=edgedriver_location)
 
 
-# 打开网页
+# open url
 webpage = r'https://baike.baidu.com/item/%E8%88%AA%E7%A9%BA%E8%88%AA%E5%A4%A9?fromModule=lemma_search-box'
 # driver.get("https://www.baidu.com")
 driver.get(webpage)
 
 
-# 查找元素
+# find elements
 meta_tags = driver.find_elements(By.XPATH,  '//meta')
 
-# 遍历meta标签，查找其中的'image'和'title'属性
+# iterate over elements
 for tag in meta_tags:
     if tag.get_attribute("name") == "image":
         image_url = tag.get_attribute("content")
@@ -29,5 +30,5 @@ for tag in meta_tags:
         title = tag.get_attribute("content")
         print("Title:", title)
 
-# 关闭浏览器
+# over
 driver.quit()
