6e789622647104014108b538590379f6b650df81
wzp <zhipangwang@gmail.com>
2023-08-02 20:47:56 +0800

    renamed:    front_end_1.py -> FrontEnd/front_end_1.py 	renamed:    front_end_2.py -> FrontEnd/front_end_2.py 	renamed:    front_end_3.py -> FrontEnd/front_end_3.py

--- FrontEnd/front_end_3.py
+++ FrontEnd/front_end_3.py
@@ -4,6 +4,19 @@ import pandas as pd
 import requests
 from typing import *
 
+# from utils import split_p2s
+
+'''
+多线程
+异常处理
+
+LOGO
+修改字体颜色，深蓝色
+对话框底边对齐
+改为英文
+
+'''
+
 
 class ServerAPI:
     @staticmethod
@@ -18,7 +31,7 @@ class ServerAPI:
         """
         # return '相关文章'
         
-        target_url = 'http://106.15.232.22:8778/search?query='
+        target_url = 'http://47.103.108.50:8002/search?query='
         url = target_url+query
         datas = requests.get(url).text
         try:
@@ -28,6 +41,7 @@ class ServerAPI:
             print('=== error datas ===')
             print(datas)
         return [p[0]for p in datas] if type(datas) == list else ['无相关信息']*5
+        # return texts if texts else '无相关信息'
 
     @staticmethod
     def ner_query(query:str) -> List[str]:
@@ -39,7 +53,10 @@ class ServerAPI:
         return:
             entities: list of str
         """
-        return ['神五', '神六']
+        target_url = 'http://47.103.108.50:8004/ner?question=' + query
+        datas = requests.post(target_url).text
+        datas = eval(datas)
+        return datas["entity_list"]
     
     @staticmethod
     def entity_linking(entities: List[str]) -> dict:
@@ -55,6 +72,7 @@ class ServerAPI:
     
     @staticmethod
     def mrc_extraction(query:str, passages:List[str]) -> Union[str, None]:
+    # def mrc_extraction(query:str, passage:str) -> Union[str, None]:
         """
         通过MRC模型，根据相关文章，抽取对应查询的答案
         
@@ -64,16 +82,24 @@ class ServerAPI:
         return:
             answer(str)
         """
-        return passages[0][1:]
+        # return passages[0][1:]
         # return None
         # jsondata = json.dumps({"question": query, "context": passage})
-        target_url = 'http://127.0.0.1:8081/mrc?question=' +query + '&context='+passage
-        # target_url = 'http://127.0.0.1:8081/mrc'
-        # url = target_url+query+'@@&&@@'+passage
-        data = requests.post(target_url).text
-        # data = requests.post(target_url, data=json.dumps({"question": query, "context": passage}, ensure_ascii=False)).text
-        data = eval(data)
-        data = data["output"]
+        if ("中国空间站" in query or "天宫号空间站" in query or "天宫空间站" in query) and len(query) <=9:
+            data = requests.get("http://localhost:8056/tiangong").text
+        elif "历史上的今天" == query or query == "历史上的今天发生了什么？":
+            mrc_answer = json.loads(requests.get("http://localhost:8056/history").text)
+            url, title = mrc_answer[0], mrc_answer[1]
+            data = "事件：" + title + "\n了解详情： " + url
+        else:
+            #####
+            passage = "".join(passages[:5])
+            passage = split_p2s(passage)
+            #####
+            target_url = 'http://47.103.108.50:8088/mrc?question=' +query + '&context='+passage
+            data = requests.post(target_url).text
+            data = eval(data)
+            data = data["output"]
         return data
 
     @staticmethod
@@ -107,40 +133,40 @@ class ServerAPI:
 
 def main_front_end():
     with gr.Blocks() as demo:
-        gr.Markdown('''航空航天问答系统
-                    ''')
+        gr.Markdown('''<div align='center' ><font size='70'>航空航天问答系统</font></div>''')
         
         with gr.Row():
             with gr.Column():
-                chatbot = gr.Chatbot(label='聊天框').style(height=600)
-                msg = gr.Textbox(placeholder='输入回车以发出信息', label='输入框')
+                chatbot = gr.Chatbot(label='Chatting Thread').style(height=600)
+                msg = gr.Textbox(placeholder='Press ENTER for chatting', label='Question \ Requirement')
                 with gr.Row():
-                    undo_button = gr.Button(value='撤销')
-                    clear_button = gr.Button(value='清除')
+                    undo_button = gr.Button(value='Withdraw')
+                    clear_button = gr.Button(value='Clear')
                     
             with gr.Column():
                 # ner&linking, retrieve, mrc
-                with gr.Accordion('命名实体识别', open=True):
-                    ner = gr.outputs.Dataframe(headers=['实体'], type='pandas')
-                with gr.Accordion('检索', open=True):
+                with gr.Accordion('PreProcessing (NER and Entity Linking)', open=True):
+                    ner = gr.outputs.Dataframe(headers=['Named Entity'], type='pandas')
+                with gr.Accordion('Retriever', open=True):
                     retrieve_components = []
                     for p in range(1, 6):
-                        with gr.Tab(f'相关信息{p}'):
-                            con = gr.Text(show_label=False, container=False)
+                        with gr.Tab(f'Top-{p} PF'):
+                            con = gr.Textbox(show_label=False, container=False, lines=8, max_lines=8)
                         retrieve_components.append(con)
-                with gr.Accordion('抽取式机器阅读理解', open=True):
+                with gr.Accordion('Extractive MRC', open=True):
                     mrc_textbox = gr.HighlightedText(
                         # value=[' ', None],
-                        # label='',
+                        label='',
                         combine_adjacent=True,
-                        container=False,
+                        # container=False,
                         # show_legend=True,
                         # show_label=False,
                     )
                     mrc_textbox.style(color_map={
-                        '抽取出的答案': 'green',
+                        'Answer': 'green',
                     })
-                with gr.Accordion('对话历史摘要', open=True):
+                    mrc_textbox.value = []
+                with gr.Accordion('对话历史摘要', open=True, visible=False):
                     qa_abstract = gr.Textbox(show_label=False, value=' ', container=False)
     
             def undo_func(chat_history):
@@ -153,9 +179,9 @@ def main_front_end():
                 return (
                     '',
                     [],
-                    pd.DataFrame({'实体':[]}),
+                    pd.DataFrame({'Named Entity':[]}),
                     *([' ']*5),
-                    ' ',
+                    [],
                     ' ',
                 )
             clear_button.click(
@@ -191,14 +217,16 @@ def main_front_end():
                             p_end = p_start+len(mrc_answer)
                             formatted.extend(map(list, zip(passage, [None]*len(passage))))
                             for q in range(p_start, p_end):
-                                formatted[q][1] = '抽取出的答案'
+                                formatted[q][1] = 'Answer'
                             return formatted
                     print('No answer')
-                    return []
+                    formatted.extend(map(list, zip(mrc_answer, ["Answer"] * len(mrc_answer))))
+                    return formatted
                         
-                chat_history.append([message, system_answer])
+                # chat_history.append([message, system_answer])
+                chat_history.append([message, mrc_answer])
                 
-                entities = pd.DataFrame({'实体':entities})
+                entities = pd.DataFrame({'Named Entity':entities})
                 passages = (passages+['无相关信息']*5)[:5]
                 return (
                     '',
@@ -221,7 +249,10 @@ def main_front_end():
                 ]
             )
             
-    demo.launch(share=True)
+    demo.launch(
+        share=False,
+        debug=False,
+    )
 
     
 if __name__ == '__main__':
